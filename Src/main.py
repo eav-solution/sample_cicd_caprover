23e8444e2cb990e76e454e11525fb49d7e4caf1b
Lam Kha Tinh <lktinh2018@gmail.com>
2024-03-16 22:20:15 +0700

    Version 2

--- Src/main.py
+++ Src/main.py
@@ -1,22 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-)
 
 
 # Define a GET operation at the root ("/") of your API
 @app.get("/")
 async def root():
     # Return a simple string directly to the user
-    return "Hello, this is sample website version 1"
+    return "Hello, this is sample website version 2"
